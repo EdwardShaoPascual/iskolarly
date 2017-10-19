c827b19573407e2fbf658cafe4eba96565e4958f
EddieVallejos <eddievallejos08@gmail.com>
2017-10-19 14:55:17 +0800

    Instantiated angular routers and views

--- backend/index.js
+++ backend/index.js
@@ -1,66 +1,52 @@
 'use strict';
 
 const config        = require(__dirname + '/config/config');
-const router	    = require(__dirname + '/router/router');
+const router        = require(__dirname + '/router/router');
 const express       = require('express');
 const body_parser   = require('body-parser');
 const helmet        = require('helmet');
 const winston       = require('winston');
-const webpack       = require('webpack');
 
 let start;
 let handler;
 let app;
-let compiler = webpack({
-    context: __dirname + "./",
-    entry: "./",
-    output: {
-        path: __dirname + "/../dist/build",
-        filename: "bundle.js"
-    }
-})
 
-compiler.run(() => {
+start = () => {
+  if (handler) {
+      handler.close();
+  }
 
-    start = () => {
-        if (handler) {
-            handler.close();
-        }
+  // create an express app
+  app = express();
 
-        // create an express app
-        app = express();
-
-        // winston instantiation
-        winston.cli();
-        winston.level = config.LOG_LEVEL || 'silly';
-        winston.log('info', 'Starting', config.APP_NAME);
+  // winston instantiation
+  winston.cli();
+  winston.level = config.LOG_LEVEL || 'silly';
+  winston.log('info', 'Starting', config.APP_NAME);
 
 
-        // setting the environment for express
-        winston.log('verbose', 'Binding 3rd-party middlewares');
-        app.set('case sensitive routing', true);
-        app.set('x-powered-by', false);
-        app.use(express.static(__dirname + '/../frontend/'));
-        app.use(require('method-override')());
-        app.use(body_parser.urlencoded({extended: true}));
-        app.use(body_parser.json());
-        app.use(require('compression')());
-        app.use(router(express.Router()));
-        app.use(helmet());
+  // setting the environment for express
+  winston.log('verbose', 'Binding 3rd-party middlewares');
+  app.set('case sensitive routing', true);
+  app.set('x-powered-by', false);
+  app.use(express.static(__dirname + '/../frontend/'));
+  app.use(require('method-override')());
+  app.use(body_parser.urlencoded({extended: true}));
+  app.use(body_parser.json());
+  app.use(require('compression')());
+  app.use(router(express.Router()));
+  app.use(helmet());
 
-        // this will start app
-        winston.log('info', 'Server listening on port', config.PORT);
-        return app.listen(config.PORT, config.IP);
-    }
-    
-    handler = start();
+  // this will start app
+  winston.log('info', 'Server listening on port', config.PORT);
+  return app.listen(config.PORT, config.IP);
+}
 
-});
-
+handler = start();
 
 
 module.exports = {
-    app,
-    start,
-    handler
+  app,
+  start,
+  handler
 }
---
+++ frontend/app.js
@@ -0,0 +1,21 @@
+'use strict'
+
+let mainApp = angular.module("app", ['ngRoute']);
+
+mainApp.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/landing.html'
+    })
+    .when('/error_404', {
+      templateUrl: 'views/error_404.html'
+    })
+    .otherwise({
+      redirectTo: '/error_404'
+    });
+
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+})
