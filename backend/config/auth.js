ce24cf92fe9621d037d2cbea6188bc583f879dbd
EddieVallejos <eddievallejos08@gmail.com>
2017-10-22 19:15:01 +0800

    Initializing auth.js

---
+++ backend/config/auth.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+  'googleAuth' : {
+    'clientID'      : 'your-secret-clientID-here',
+    'clientSecret'  : 'your-client-secret-here',
+    'callbackURL'   : 'http://localhost:8000/auth/google/callback'
+  }
+};
