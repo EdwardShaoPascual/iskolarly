ee9707f20edfcf569d8c4f592acca757ce8965b9
EddieVallejos <eddievallejos08@gmail.com>
2018-03-07 10:40:21 +0800

    Fixed login and sign up functionalities

--- backend/controller/landing.controller.js
+++ backend/controller/landing.controller.js
@@ -16,7 +16,11 @@ exports.login = function(req, res, next) {
       return res.status(500).send({message: "An error has encountered"})
     } else if (result.length === 1) {
       bcrypt.compare(payload.password, result[0].password, function(err, response) {
-        return res.send(response)
+        if (response === true) {
+          return res.status(200).send(result)
+        } else if (response === false) {
+          return res.status(404).send({message: "Invalid email address or password"})
+        }
       });
     } else if (result.length === 0) {
       return res.status(404).send({message: "Invalid email address or password"})
