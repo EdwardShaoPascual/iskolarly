a738a6f2cdddfeaf104ed03fea175dd9dd0161d8
EddieVallejos <eddievallejos08@gmail.com>
2018-03-07 10:54:27 +0800

    Fixes in packages

--- frontend/services/landing.service.js
+++ frontend/services/landing.service.js
@@ -1,60 +1,59 @@
 'use strict';
 
 (() => {
-	angular
-		.module('app')
-		.factory('landing-service', landing_service);
+  angular
+    .module('app')
+    .factory('landing-service', landing_service);
 
-		landing_service.$inject = ['$window', '$http', '$q'];
+    landing_service.$inject = ['$window', '$http', '$q'];
 
-		const headers = {
-			'content-type': 'application/x-www-form-urlencoded'
-		};
+    const headers = {
+      'content-type': 'application/x-www-form-urlencoded'
+    };
 
-		function landing_service($window, $http, $q) {
-		
-			const user_login = function (data) {
-				let deferred = $q.defer();
-
+    function landing_service($window, $http, $q) {
+    
+      const user_login = function (data) {
+        let deferred = $q.defer();
         $http({
-					method: 'POST',
-					params: data,
-					xhrFields: {withCredentials: true},
-					url: '/api/login',
-					headers: headers
-				})
-				.then(function(res) {
-					$window.location.href = '/#/home';
-					deferred.resolve(res.data);
-				}, function(err) {
-					deferred.reject(err);
-				})
+          method: 'POST',
+          params: data,
+          xhrFields: {withCredentials: true},
+          url: '/api/login',
+          headers: headers
+        })
+        .then(function(res) {
+          $window.location.href = '/#/home';
+          deferred.resolve(res.data);
+        }, function(err) {
+          deferred.reject(err);
+        })
 
-				return deferred.promise;
-			}
+        return deferred.promise;
+      }
 
-			const sign_up = function (data) {
-				let deferred = $q.defer();
-				$http({
-					method: 'POST',
-					params: data,
-					xhrFields: {withCredentials: true},
-					url: '/api/register',
-					headers: headers
-				})
-				.then(function(res) {
-					deferred.resolve(res.data);
-				}, function(err) {
-					deferred.reject(err);
-				})
+      const sign_up = function (data) {
+        let deferred = $q.defer();
+        $http({
+          method: 'POST',
+          params: data,
+          xhrFields: {withCredentials: true},
+          url: '/api/register',
+          headers: headers
+        })
+        .then(function(res) {
+          deferred.resolve(res.data);
+        }, function(err) {
+          deferred.reject(err);
+        })
 
-				return deferred.promise;
-			}
-			
-			let service = {};
-			service.user_login 				= user_login;
-			service.sign_up 				= sign_up;
-			return service;
+        return deferred.promise;
+      }
+      
+      let service = {};
+      service.user_login 				= user_login;
+      service.sign_up 				  = sign_up;
+      return service;
 
-		}
+    }
 })();
