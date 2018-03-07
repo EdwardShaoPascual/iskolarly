d60b85e766e11c34293b7690f39340d974efe249
EddieVallejos <eddievallejos08@gmail.com>
2018-03-07 13:43:13 +0800

    Fixed UI and UX in signing up

--- frontend/controllers/landing.controller.js
+++ frontend/controllers/landing.controller.js
@@ -42,10 +42,29 @@
     }
 
     $scope.register = () => {
-
-      if ($scope.registerData.username.length < 6) {
-        toastr.error("Username must be in length of at least 6 characters", 'Error');        
-      } else {
+      $scope.registerData.username = $scope.registerData.username.replace(" ", "");
+      let pattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      if ($scope.registerData.firstname === "" || 
+          $scope.registerData.middlename === "" ||
+          $scope.registerData.lastname === "" ||
+          $scope.registerData.email === "" || 
+          $scope.registerData.username === "" ||
+          $scope.registerData.password === "" ||
+          $scope.registerData.course === "" ||
+          $scope.registerData.birthday === "" ||
+          $scope.registerData.college === "") {
+        toastr.error("Please fill all the missing fields!", 'Error');                         
+      } 
+      else if (pattern.test($scope.registerData.email) === false) {
+        toastr.error("Invalid input of email address!", 'Error');             
+      }
+      else if ($scope.registerData.username.length < 6) {
+        toastr.error("Username must be in length of at least 6 characters!", 'Error');     
+      } 
+      else if ($scope.registerData.password.length < 8) {
+        toastr.error("Password must be in length of at least 8 characters!", 'Error');     
+      } 
+      else {
         landing_service
         .sign_up($scope.registerData)
         .then(function(res) {
