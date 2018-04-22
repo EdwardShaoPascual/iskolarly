b320eea92819f965b10a76b5a18dc7d5d75ab8c9
EddieVallejos <eddievallejos08@gmail.com>
2018-04-22 13:32:18 +0800

    Changes in add quiz

--- frontend/controllers/course.controller.js
+++ frontend/controllers/course.controller.js
@@ -157,25 +157,34 @@
       })
 		}
 
-		$scope.questionnaires_add = () => {
+		$scope.questionnaires_add = (data) => {
       $scope.questionnairesData.course_id = window.location.href.split("/")[5];
       $scope.questionnairesData.post = $scope.note_info.post;
       CourseService
 			.add_questionnaires($scope.questionnairesData)
 			.then(function(res) {
-        let data = {
-          questionnaire_id: '',
-          questionnaire_name: '',
-          questionnaire_desc: '',
-          questionnaire_no: ''
-        }
-        data.questionnaire_id = res.insertId;
-        data.questionnaire_name = $scope.questionnairesData.questionnaire_name;
-        data.questionnaire_desc = $scope.questionnairesData.questionnaire_desc;
-        data.questionnaire_no = $scope.questionnairesData.questionnaire_no;
         toastr.success("The quiz has been added", 'Success');
-        $('#addQuestionnaire').modal('hide');
-        $scope.user.push(data);
+        $timeout(() => {
+          let time = moment().format('ll').split(',')[0];
+          let urls = /(\b(https?|ftp):\/\/[A-Z0-9+&@#\/%?=~_|!:,.;-]*[-A-Z0-9+&@#\/%=~_|])/gim;
+          if ($scope.note_info.post.match(urls)) {
+            $scope.note_info.post = $scope.note_info.post.replace(urls, "<a href=\"$1\" target=\"_blank\">$1</a>")
+          }
+          let datum = {
+            post: $scope.note_info.post,
+            firstname: data.firstname,
+            lastname: data.lastname,
+            time_posted: time,
+            questionnaire_id: res.insertId,
+            questionnaire_name: $scope.questionnairesData.questionnaire_name,
+            questionnaire_desc: $scope.questionnairesData.questionnaire_desc,
+            questionnaire_no: $scope.questionnairesData.questionnaire_no
+          }
+          $scope.announcements.unshift(datum);
+          $scope.$apply();
+          $('.modal').modal('hide');          
+          $scope.note_info.post = '';                   
+        }, 100);
 			}, function(err) {
         toastr.error(err.data, 'Error');
 			})
