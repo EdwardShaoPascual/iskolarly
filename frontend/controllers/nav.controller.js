d5a42388b985c6a3bcfc99c3c1c1588a00daddf0
EddieVallejos <eddievallejos08@gmail.com>
2018-05-02 09:18:39 +0800

    Fixed nav issues and relocation

--- frontend/controllers/nav.controller.js
+++ frontend/controllers/nav.controller.js
@@ -7,7 +7,17 @@
 
 		function NavCtrl($scope, $window, $rootScope, $location, $interval) {
       
+      $scope.home = true;
+      $scope.courses = false;
+
       $scope.checkURL = () => {
+        if ($location.path() == "/home") {
+          $scope.home = true;
+          $scope.courses = false;
+        } else if ($location.path() == "/courses") {
+          $scope.home = false;
+          $scope.courses = true;
+        }
         if ($location.path() == "/")
           return false;
         else return true;
